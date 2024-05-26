620d70d9241d8aef51747b4e8fceec46cefbd4bf
Sarath Kumar Dunga <sarathkumardunga09@gmail.com>
2024-05-26 12:18:18 -0700

    Created middleware for authRRoutes - login, signup..

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
 const app = express();
 
 dotenv.config();
@@ -10,6 +11,9 @@ app.get("/", (req, res) => {
     //root route http://localhost:8000/
     res.send("Hello testing the SERVER!");
 });
+
+//Middleware to call the authRoutes whenever /api/auth is visited
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
