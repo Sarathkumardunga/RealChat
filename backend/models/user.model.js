a3b8a4a97273bbf9b9ea67a4b00a3806eeb1adf9
Sarath Kumar Dunga <sarathkumardunga09@gmail.com>
2024-05-27 16:13:35 -0700

    Added userSchema in models

---
+++ backend/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+//Creating the schema
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ["male", "female"]
+    },
+    profilePic: {
+        type: String,
+        default: "",
+    }
+});
+
+//Now create a user model based on the schema
+const User = mongoose.model("User", userSchema);
+
+export default User;
--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,22 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT | 5000;
 
-// Testing the server
-app.get("/", (req, res) => {
-    //root route http://localhost:8000/
-    res.send("Hello testing the SERVER!");
-});
+dotenv.config();
+
+//Middleware to parse the incoming requests with JSON payloads from req.body
+app.use(express.json()); 
 
 //Middleware to call the authRoutes whenever /api/auth is visited
 app.use("/api/auth", authRoutes);
+
+// // Testing the server
+// app.get("/", (req, res) => {
+//     //root route http://localhost:8000/
+//     res.send("Hello testing the SERVER!");
+// });
+
 
 app.listen(PORT, () => {
     connectToMongoDB();
