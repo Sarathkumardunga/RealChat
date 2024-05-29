e1b6a8601c09e20f9cf2f73eb95eb987e1a48b08
Sarath Kumar Dunga <sarathkumardunga09@gmail.com>
2024-05-29 10:47:52 -0700

    Hashed the password in signup and created login functionality.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 // Logics for each callback controller from auth.routes.js
 // To make the file organized in a more clean way
 
+import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import generateTokenandSetCookie from "../utils/generateToken.js";
 
@@ -19,6 +20,8 @@ export const signup = async (req, res) => {
         }
 
         //Will create hash password here
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         //Free avatarApi for profile pics
         //https://avatar-placeholder.iran.liara.run/
@@ -29,7 +32,7 @@ export const signup = async (req, res) => {
         const newUser = new User({
             fullName,
             username,
-            password,
+            password: hashedPassword,
             gender,
             profilePic: gender === "male" ? boyProfilePic : girlProfilePic
         })
@@ -59,8 +62,32 @@ export const signup = async (req, res) => {
     }
 };
 
-export const login = (req, res) => {
-    console.log("LoginUser");
+export const login = async (req, res) => {
+    try {
+        const {username, password} = req.body;
+        const user = await User.findOne({username});
+
+        //while comparing, we need to add || "" at last as bcrypt returns password - undefined when the user is not present
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+        //console.log("Password match:", isPasswordCorrect); 
+
+        if(!user || !isPasswordCorrect) {
+            return res.status(400).json({error: "Invalid username or password"});
+        }
+
+        //if the user is valid, generate token and store in cookie
+        generateTokenandSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic,
+        });
+    } catch (err) {
+        console.log("Error in login controller", err.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
 }
 
 export const logout = (req, res) => {
